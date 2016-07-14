1bfcce215e24df85b68f381547b96ac853e8a025
Alex Muramoto <amuramoto@apache.org>
2016-07-14 15:56:31 -0700

    Add working sliders for rgba and brightness

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,6 +1,7 @@
 import {Component, ViewChild, ElementRef} from '@angular/core';
 import {NavController} from 'ionic-angular';
 import { Camera } from 'ionic-native';
+import { cloneDeep } from 'lodash';
 
 @Component({
   templateUrl: 'build/pages/home/home.html'
@@ -12,40 +13,101 @@ export class HomePage {
 
 	private photoCanvas: any;
 	private photoCtx: CanvasRenderingContext2D;
+private canvasCopy;
+  private ctxCopy;
+  private canvasWidth;
+  private canvasHeight;
 	private image;
+	private imageData;
 	private showTable: Boolean;
+	private sliders: Array<Object>;
 	private rows: Array<string>;
 	private columns: Array<string>;
-
-
+	private red: number;
+	private green: number;
+	private blue: number;
+	private alpha: number;	
+private brightness: number;
+private test2: any;
   constructor(private navController: NavController) {
-  	this.showTable = true;
+  	this.showTable = true;  	
+  	this.red = this.green = this.blue = this.alpha = 100;  	
+    this.brightness=0;
   }
 
   ngAfterViewInit () {
-  	this.drawImageToCanvas('./img/default.jpg');
-  }
-
-  private drawImageToCanvas (imageUri) {
-
   	this.photoCanvas = this.photo.nativeElement;
   	this.photoCtx = this.photoCanvas.getContext("2d");    		
+   	
+  	this.drawImage('./img/default.jpg');
+
+  }
+	
+
+private redraw (callback) {  
+  let imageDataCopy = this.ctxCopy.getImageData(0,0,this.canvasWidth, this.canvasHeight);
+  let d = imageDataCopy.data;
+  for (var i = 0; i < d.length; i += 8) {
+      
+    
+        d[i] *= this.red/100 + this.brightness/100
+    
+        d[i+1] *= this.green/100 + this.brightness/100;
+    
+        d[i+2] *= this.blue/100 + this.brightness/100;
+    
+        d[i+3] *= this.alpha/100 + this.brightness/100;
+    
+    }
+  callback.call(this);
+  
+}
+
+private calcPixels (d) {
+  
+  
+  // this.photoCtx.putImageData(imageDataCopy, 0, 0);  
+}
+
+	private updateColor() {	
+    this.redraw(this.calcPixels);
+
+
+    
+    
+    
+
+
+    
+	}
+
+
+  private drawImage (imageUri) {
+  	
   	this.image = new Image();
 
   	this.image.onload = () => {
- 
-  		let canvasWidth = window.innerWidth - 40;
-  		let canvasHeight = (canvasWidth / this.image.width) * this.image.height - 40 ; //match canvas aspect ratio to original image
+			
+  		this.canvasWidth = window.innerWidth - 40;
+  		this.canvasHeight = (this.canvasWidth / this.image.width) * this.image.height - 40 ; //match canvas aspect ratio to original image
   	
-  		this.photoCtx.canvas.width = canvasWidth;
-  		this.photoCtx.canvas.height = canvasHeight;
+  		this.photoCtx.canvas.width = this.canvasWidth;
+  		this.photoCtx.canvas.height = this.canvasHeight;
   		
   		this.photoCtx.drawImage(this.image, 0, 0, this.image.width - 40, this.image.height - 40,
-  												 0, 0, canvasWidth, canvasHeight);
-  	
+  												 0, 0, this.canvasWidth, this.canvasHeight);
+
+      this.canvasCopy = this.photoCanvas.cloneNode();
+    this.ctxCopy = this.canvasCopy.getContext("2d");
+    this.ctxCopy.drawImage(this.image, 0, 0, this.image.width - 40, this.image.height - 40,
+                           0, 0, this.canvasWidth, this.canvasHeight)
+
   	}
 
   	this.image.src=imageUri;
+
+
+    
 
   }
 
@@ -61,7 +123,7 @@ export class HomePage {
       correctOrientation: true
     })
   	.then((file) => {    	
-      this.drawImageToCanvas(file);
+      this.drawImage(file);
     }, (err) => {
       console.log(err);
     });

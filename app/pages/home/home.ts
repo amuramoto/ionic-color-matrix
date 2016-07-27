03b632833d69133e36c3303af39b06617c19005c
Alex Muramoto <amuramoto@apache.org>
2016-07-27 15:43:08 -0500

    Trying debounce

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -28,23 +28,27 @@ private canvasCopy;
 	private alpha: number;	
 private brightness: number;
 private test2: any;
+private start;
   constructor(private navController: NavController) {
   	this.showTable = true;  	
   	this.red = this.green = this.blue = this.alpha = 100;  	
     this.brightness=0;
   }
 
-  ngAfterViewInit () {
+  ionViewLoaded () {
   	this.photoCanvas = this.photo.nativeElement;
   	this.photoCtx = this.photoCanvas.getContext("2d");    		
    	
   	this.drawImage('./img/default.jpg');
-
+    
   }
 	
+
 private redraw () {  
-  let imageDataCopy = this.ctxCopy.getImageData(0,0,this.canvasWidth, this.canvasHeight);
-  let d = imageDataCopy.data;
+  if (!this.start || Date.now() - this.start > 50) {
+    this.start = Date.now();
+  let imageData = this.ctxCopy.getImageData(0,0,this.canvasWidth, this.canvasHeight);
+  let d = imageData.data;
     
   for (var i = 0; i < d.length; i += 4) {
    
@@ -57,9 +61,31 @@ private redraw () {
     d[i+3] *= this.alpha/100 + this.brightness/100;
   
   }
+
+  this.photoCtx.putImageData(imageData, 0, 0);
+  }
+}
+
+
+private test = (timestamp) => {
+  let imageData = this.ctxCopy.getImageData(0,0,this.canvasWidth, this.canvasHeight);
+  let d = imageData.data;
     
-  this.photoCtx.putImageData(imageDataCopy, 0, 0);
+  for (var i = 0; i < d.length; i += 4) {
+   
+    d[i] *= this.red/100 + this.brightness/100
+
+    d[i+1] *= this.green/100 + this.brightness/100;
+
+    d[i+2] *= this.blue/100 + this.brightness/100;
+
+    d[i+3] *= this.alpha/100 + this.brightness/100;
   
+  }
+
+  this.photoCtx.putImageData(imageData, 0, 0);
+
+  requestAnimationFrame(this.test)
 }
 
 private calcPixels (imageDataCopy) {
